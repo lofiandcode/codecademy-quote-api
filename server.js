29c6e0388dbef06a0a6067dcffc8d444c1e5edaa
lofiandcode <josephtrettevik@gmail.com>
2022-04-12 20:29:39 -0700

    Add get routes for /api/quotes/random and /api/quotes

--- server.js
+++ server.js
@@ -8,3 +8,28 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
+
+app.get('/api/quotes/random', (req, res, next) => {
+    const randomQuote = getRandomElement(quotes);
+    res.send(randomQuote);
+});
+
+app.get('/api/quotes', (req, res, next) => {
+    const query = req.query;
+    console.log(query)
+    if (Object.keys(query).length != 0){
+        console.log('in outer first if')
+        if (query.hasOwnProperty('person')) {
+            const filteredQuotes = quotes.filter(quote => quote.person === query.person);
+            res.send(filteredQuotes);
+        } else {
+            console.log("Error: Your query must contain a 'person' key")
+        }
+    } else {
+        console.log('in outer else')
+        res.send(quotes);
+    }
+})
